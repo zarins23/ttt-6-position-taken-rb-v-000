46e46e36bf02b4eba4c05b73661472cc74b9b0f8
Zarin Shahab <zarins23@gmail.com>
2018-02-17 01:44:31 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,9 @@
 def position_taken?(board, index)
    if board[index] == " " || board[index] == "" || board[index] == nil
      false
-   else true
+   elsif board[index] == "X" || board[index] == "O"
+     true
+   else
+     false
    end
 end
