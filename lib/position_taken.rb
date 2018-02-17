beb49f920dd72365216b0229fea404569b8e6843
Zarin Shahab <zarins23@gmail.com>
2018-02-17 01:43:07 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 def position_taken?(board, index)
    if board[index] == " " || board[index] == "" || board[index] == nil
      false
-   elsif board[index] == "X" || board[index] == "O"
+   elsif board[index] == " X " || board[index] == " O "
      false
    else
      true
